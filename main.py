1d4ad2817f51044711bdcad1979d5a0a47359b45
Jennicos <jenefferaguiar532@gmail.com>
2026-03-12 11:08:53 -0300

    feat: melhorias em parte do programa
    Aperfeiçoamento de uma funcionalidade existente, com ajustes na lógica e organização do código para melhor desempenho e manutenção.

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ while True:
     print("Opções:")
     print("1- Cadastro")
     print("2- Login")
-    opcoes = int(input("Escolha uma opção: "))
-
+    
+    try:
+        opcoes = int(input("Escolha uma opção: "))
+    except ValueError:
+        print("\n[ERRO] Entrada inválida! Digite apenas os números das opções.")
+        continue
     match opcoes:
         case 1:
             print("\n--- CADASTRO ---")
@@ -16,3 +20,17 @@ while True:
                 senha = input("Crie uma senha: ")
                 usuarios[novo_usuario] = senha 
                 print("Cadatro rea lizado com sucesso!")               
+        case 2:
+            print("\n--- LOGIN ---")
+            nome_login = input("Digite seu nome de usuario: ")
+
+            if nome_login not in usuarios:
+                print("Este usuario não existe.")
+            else:
+                senha_login = input("Digite a sua senha: ")
+
+                if senha_login == ususarios[nome_login]:
+                    print(f"Login realizado com sucesso! Bem-vindo(a), {nome_login}!")
+
+                else:
+                    print("senha incorreta.")             
